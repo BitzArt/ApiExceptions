9303c6eb862439b8cc314175e749f16d27cc79b0
Yuriy Durov <u.a.durov@gmail.com>
2021-07-31 11:12:11 +0300

    Add NoContentException

---
+++ src/Core/Exceptions/NoContentException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace BitzArt.ApiExceptions
+{
+    public class NoContentException : ApiException
+    {
+        public NoContentException(string message) : base(HttpStatusCode.NoContent, message) { }
+    }
+}
