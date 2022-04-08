104e6861b1df73fd846fab4ef814b5554bfe3698
Yuriy Durov <u.a.durov@gmail.com>
2022-04-08 21:04:39 +0300

    Expose HttpStatusCode

--- src/Core/Exceptions/Base/ApiExceptionBase.cs
+++ src/Core/Exceptions/Base/ApiExceptionBase.cs
@@ -5,7 +5,7 @@ namespace BitzArt.ApiExceptions
 {
     public class ApiExceptionBase : Exception
     {
-        protected HttpStatusCode StatusCode { get; set; }
+        public HttpStatusCode StatusCode { get; private set; }
 
         protected ApiExceptionBase(HttpStatusCode statusCode, string message) : base(message)
         {
