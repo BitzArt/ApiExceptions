e3540b1c894829a9f129560f2e87ad269470a3d5
Yuriy Durov <u.a.durov@gmail.com>
2022-07-05 19:14:20 +0400

    Remove Core Project

---
+++ sample/BitzArt.ApiExceptions.AspNetCore.Sample/Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BitzArt.ApiExceptions.AspNetCore.Sample.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("notfound")]
+        public IActionResult ThrowNotFound()
+        {
+            throw ApiException.NotFound("sample 'not found' exception message");
+        }
+
+        [HttpGet("unauthorized")]
+        public IActionResult ThrowUnauthorized()
+        {
+            throw ApiException.Unauthorized("sample 'unauthorized' exception message");
+        }
+
+        [HttpGet("custom/{statusCode}")]
+        public IActionResult ThrowCustom([FromRoute] HttpStatusCode statusCode)
+        {
+            throw ApiException.Custom(statusCode, "sample custom status code");
+        }
+    }
+}
--- src/Implementations/BitzArt.ApiExceptions.AspNetCore/Exceptions/Base/ApiExceptionBase.cs
+++ src/Implementations/BitzArt.ApiExceptions.AspNetCore/Exceptions/Base/ApiExceptionBase.cs
@@ -7,7 +7,14 @@ namespace BitzArt.ApiExceptions
     {
         public HttpStatusCode StatusCode { get; private set; }
 
-        protected ApiExceptionBase(HttpStatusCode statusCode, string message) : base(message)
+        public string Type { get; private set; }
+
+        public string Detail { get; private set; }
+
+        protected ApiExceptionBase(
+            HttpStatusCode statusCode,
+            string message
+            ) : base(message)
         {
             StatusCode = statusCode;
         }
