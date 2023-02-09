a5d9ce0a7378ccbdc2366d3cd388bce7d31ab6ec
Yuriy Durov <u.a.durov@gmail.com>
2023-02-09 23:56:05 +0400

    Add ExceptionExtensions

---
+++ src/BitzArt.ApiExceptions.AspNetCore/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BitzArt.ApiExceptions;
+using System.Net;
+
+namespace System;
+
+public static class ExceptionExtensions
+{
+    public static int GetHttpStatusCode(this Exception exception)
+    {
+        if (exception is ApiExceptionBase apiException) return apiException.StatusCode;
+        return (int)HttpStatusCode.InternalServerError;
+    }
+}
